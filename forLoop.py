d76db152e283b90d24a3c2f1be7e65703d25912f
NguyenVinhHung2705 <2705oniichan@gmail.com>
2024-11-22 23:14:17 +0700

    add demo thử

---
+++ forLoop.py
@@ -0,0 +1,13 @@
+words = list()
+for i in range(3):
+    words.append(input())
+s = input()
+s = s.upper()
+if len(words) == len(s):
+    for i in range(len(words)):
+        if words[i][0] != s[0]:
+            break
+    print("True")
+
+else:
+    print("False")
