e12f4ceac48ce5d4992ca3e27ef38b26883ddc23
NguyenVinhHung2705 <2705oniichan@gmail.com>
2024-12-12 17:06:03 +0700

    add

---
+++ 657. Robot Return to Origin.py.py
@@ -0,0 +1,18 @@
+#657 leetcode
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        RBx = 0
+        RBy = 0
+        move_list = {
+            "L" : -1,
+            "R" : 1,
+            "U" : 1,
+            "D" : -1
+        }
+        for move in moves:
+            if move == "L" or move == "R":
+                RBx += move_list[move]
+            else:
+                RBy += move_list[move]
+        return True if RBx == 0 and RBy == 0 else False
